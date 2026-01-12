7f85a7d15f8d941b7d16e0366b2cdde84cc38e45
Genta Indra Winata <genta@mac.mynetworksettings.com>
2026-01-12 03:45:59 -0500

    add args

--- generate_codemix_data.py
+++ generate_codemix_data.py
@@ -1,121 +1,129 @@
+import argparse
 import copy
 import json
 import os
 from datasets import load_dataset
-from tqdm import tqdm
 from openai import OpenAI
 from pydantic import BaseModel
+from tqdm import tqdm
 
-openai_api_key = "<OPENAI_KEY>"
-client = OpenAI(timeout=1500, api_key=openai_api_key)
 
-ds = load_dataset("google/simpleqa-verified")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Parser")
+    parser.add_argument("--openai_key", type=str, default="mc", help="Type of question to evaluate (oe or mc)")\
 
-problems = []
-for i in range(len(ds["eval"])):
-    problem = ds["eval"][i]["problem"]
-    original_index = ds["eval"][i]["original_index"]
-    id = original_index
-    problems.append({"id":id, "problem":problem})
+    args = parser.parse_args()
 
-class AnswerFormat(BaseModel):
-    explanation: str
-    answer: str
+    openai_api_key = args.openai_key
+    client = OpenAI(timeout=1500, api_key=openai_api_key)
 
-random_percentages_list = ["50"]
-selective_percentages_list = ["50"]
-languages_list = ["Klingon","Dothraki","Afrikaans","Albanian","Amharic","Arabic","Armenian","Azerbaijani","Burmese","Danish","Dutch","Finnish","French","Georgian","German","Greek","Hebrew","Hungarian","Icelandic","Javanese","Kannada","Khmer","Korean","Latvian","Malay","Malayalam","Mongolian","Norwegian","Persian","Polish","Portuguese","Romanian","Russian","Slovenian","Sundanese","Swahili","Traditional Chinese","Swedish","Tagalog","Tamil","Telugu","Thai","Turkish","Vietnamese","Welsh","Japanese","Indonesian","Italian","Simplified Chinese","Hindi","Marathi","Spanish"]
+    ds = load_dataset("google/simpleqa-verified")
 
-NUM_SPLIT = 20
-SAMPLES_PER_SPLIT = len(problems) // NUM_SPLIT
+    problems = []
+    for i in range(len(ds["eval"])):
+        problem = ds["eval"][i]["problem"]
+        original_index = ds["eval"][i]["original_index"]
+        id = original_index
+        problems.append({"id":id, "problem":problem})
 
-for split in range(10, NUM_SPLIT):
-    min_id = split * SAMPLES_PER_SPLIT
-    max_id = (split+1) * SAMPLES_PER_SPLIT
-    if split == NUM_SPLIT-1:
-        max_id = len(problems)
+    class AnswerFormat(BaseModel):
+        explanation: str
+        answer: str
 
-    for language_list in languages_list:
-        if type(language_list) is list:
-            languages = "_".join(lang for lang in language_list)
-        else:
-            languages = language_list
-            language_list = [language_list]
+    random_percentages_list = ["50"]
+    selective_percentages_list = ["50"]
+    languages_list = ["Klingon","Dothraki","Afrikaans","Albanian","Amharic","Arabic","Armenian","Azerbaijani","Burmese","Danish","Dutch","Finnish","French","Georgian","German","Greek","Hebrew","Hungarian","Icelandic","Javanese","Kannada","Khmer","Korean","Latvian","Malay","Malayalam","Mongolian","Norwegian","Persian","Polish","Portuguese","Romanian","Russian","Slovenian","Sundanese","Swahili","Traditional Chinese","Swedish","Tagalog","Tamil","Telugu","Thai","Turkish","Vietnamese","Welsh","Japanese","Indonesian","Italian","Simplified Chinese","Hindi","Marathi","Spanish"]
 
-        os.system(f"mkdir -p \"output/{languages}/\"")
+    NUM_SPLIT = 20
+    SAMPLES_PER_SPLIT = len(problems) // NUM_SPLIT
 
-        print(languages, "from", languages_list, "split", split, "min_id", min_id, "max_id", max_id)
-        outputs = {}
-        outputs[languages] = {}
-        for i in tqdm(range(min_id, max_id)):
-            all_responses = {"original": problems[i]["problem"]}
-            text = problems[i]["problem"]
+    for split in range(10, NUM_SPLIT):
+        min_id = split * SAMPLES_PER_SPLIT
+        max_id = (split+1) * SAMPLES_PER_SPLIT
+        if split == NUM_SPLIT-1:
+            max_id = len(problems)
 
-            for percentage in random_percentages_list:
-                all_responses[percentage + "_random"] = []
+        for language_list in languages_list:
+            if type(language_list) is list:
+                languages = "_".join(lang for lang in language_list)
+            else:
+                languages = language_list
+                language_list = [language_list]
 
-                for j in range(1):
-                    completion = client.chat.completions.parse(
-                        model="gpt-5.2",
-                        reasoning_effort="low",
-                        messages=[
-                            {"role": "developer", "content": "You are a multilingual speaker."},
-                            {"role": "user", "content": f"""Given an English text, produce a code-switched version with roughly about {percentage}% of the words or phrases into {languages}, while preserving the original meaning.\nApply code-switching by random. Don't write new punctuation if not needed. The answer must not have any preamble.\nEnglish text: {text}"""}
-                        ],
-                        service_tier="flex",
-                        timeout=1500,
-                        response_format=AnswerFormat
-                    )
-                    
-                    output = completion.choices[0].message.content
-                    all_responses[percentage + "_random"].append(output)
+            os.system(f"mkdir -p \"output/{languages}/\"")
 
-            for percentage in selective_percentages_list:
-                all_responses[percentage + "_selective"] = []
-                for lang in language_list:
-                    all_responses["translate_" + lang] = []
-                    all_responses[percentage + "_grammarforce_" + lang] = []
-                    all_responses[percentage + "_backtranslate_grammarforce_" + lang] = []
-                all_responses[percentage + "_grammarforce_English"] = []
-                all_responses[percentage + "_backtranslate_grammarforce_English"] = []
-                all_responses[percentage + "_backtranslate"] = []
+            print(languages, "from", languages_list, "split", split, "min_id", min_id, "max_id", max_id)
+            outputs = {}
+            outputs[languages] = {}
+            for i in tqdm(range(min_id, max_id)):
+                all_responses = {"original": problems[i]["problem"]}
+                text = problems[i]["problem"]
 
-                for j in range(1):
-                    completion = client.chat.completions.parse(
-                        model="gpt-5.2",
-                        reasoning_effort="low",
-                        messages=[
-                            {"role": "developer", "content": "You are a multilingual speaker."},
-                            {"role": "user", "content": f"""Given an English text, produce a code-switched version with roughly about {percentage}% of the words or phrases into {languages}, while preserving the original meaning.\nApply code-switching selectively, BUT always try to code-switch if possible, so that the final output naturally mixes English with the target language(s). Don't write new punctuation if not needed. The answer must not have any preamble.\nEnglish text: {text}"""}
-                        ],
-                        service_tier="flex",
-                        timeout=1500,
-                        response_format=AnswerFormat
-                    )
-                    
-                    output = completion.choices[0].message.content
-                    all_responses[percentage + "_selective"].append(output)
+                for percentage in random_percentages_list:
+                    all_responses[percentage + "_random"] = []
 
-                    for lang_id in range(len(language_list)):
-                        lang = language_list[lang_id]
+                    for j in range(1):
+                        completion = client.chat.completions.parse(
+                            model="gpt-5.2",
+                            reasoning_effort="low",
+                            messages=[
+                                {"role": "developer", "content": "You are a multilingual speaker."},
+                                {"role": "user", "content": f"""Given an English text, produce a code-switched version with roughly about {percentage}% of the words or phrases into {languages}, while preserving the original meaning.\nApply code-switching by random. Don't write new punctuation if not needed. The answer must not have any preamble.\nEnglish text: {text}"""}
+                            ],
+                            service_tier="flex",
+                            timeout=1500,
+                            response_format=AnswerFormat
+                        )
+                        
+                        output = completion.choices[0].message.content
+                        all_responses[percentage + "_random"].append(output)
 
-                        for gf_lang in language_list + ["English"]:
-                            completion = client.chat.completions.parse(
-                                model="gpt-5.2",
-                                reasoning_effort="low",
-                                messages=[
-                                    {"role": "developer", "content": "You are a multilingual speaker."},
-                                    {"role": "user", "content": f"""Given an English text, produce a code-switched version with roughly about {percentage}% of the words or phrases into {lang}, while preserving the original meaning.\nApply code-switching selectively, BUT always try to code-switch if possible, so that the final output naturally mixes English with the target language(s) and random the code-switched text to follow {gf_lang} grammar. Don't write new punctuation if not needed. The answer must not have any preamble.\nEnglish text: {text}"""}
-                                ],
-                                service_tier="flex",
-                                timeout=1500,
-                                response_format=AnswerFormat
-                            )
-                            
-                            output = completion.choices[0].message.content
-                            all_responses[percentage + "_grammarforce_" + gf_lang].append(output)
+                for percentage in selective_percentages_list:
+                    all_responses[percentage + "_selective"] = []
+                    for lang in language_list:
+                        all_responses["translate_" + lang] = []
+                        all_responses[percentage + "_grammarforce_" + lang] = []
+                        all_responses[percentage + "_backtranslate_grammarforce_" + lang] = []
+                    all_responses[percentage + "_grammarforce_English"] = []
+                    all_responses[percentage + "_backtranslate_grammarforce_English"] = []
+                    all_responses[percentage + "_backtranslate"] = []
 
-            outputs[languages][i] = all_responses
+                    for j in range(1):
+                        completion = client.chat.completions.parse(
+                            model="gpt-5.2",
+                            reasoning_effort="low",
+                            messages=[
+                                {"role": "developer", "content": "You are a multilingual speaker."},
+                                {"role": "user", "content": f"""Given an English text, produce a code-switched version with roughly about {percentage}% of the words or phrases into {languages}, while preserving the original meaning.\nApply code-switching selectively, BUT always try to code-switch if possible, so that the final output naturally mixes English with the target language(s). Don't write new punctuation if not needed. The answer must not have any preamble.\nEnglish text: {text}"""}
+                            ],
+                            service_tier="flex",
+                            timeout=1500,
+                            response_format=AnswerFormat
+                        )
+                        
+                        output = completion.choices[0].message.content
+                        all_responses[percentage + "_selective"].append(output)
 
-        with open(f'output/{languages}/{languages}_split_{split}.json', 'w', encoding="utf-8") as f:
-            json.dump(outputs, f, indent=4, ensure_ascii=False)
+                        for lang_id in range(len(language_list)):
+                            lang = language_list[lang_id]
+
+                            for gf_lang in language_list + ["English"]:
+                                completion = client.chat.completions.parse(
+                                    model="gpt-5.2",
+                                    reasoning_effort="low",
+                                    messages=[
+                                        {"role": "developer", "content": "You are a multilingual speaker."},
+                                        {"role": "user", "content": f"""Given an English text, produce a code-switched version with roughly about {percentage}% of the words or phrases into {lang}, while preserving the original meaning.\nApply code-switching selectively, BUT always try to code-switch if possible, so that the final output naturally mixes English with the target language(s) and random the code-switched text to follow {gf_lang} grammar. Don't write new punctuation if not needed. The answer must not have any preamble.\nEnglish text: {text}"""}
+                                    ],
+                                    service_tier="flex",
+                                    timeout=1500,
+                                    response_format=AnswerFormat
+                                )
+                                
+                                output = completion.choices[0].message.content
+                                all_responses[percentage + "_grammarforce_" + gf_lang].append(output)
+
+                outputs[languages][i] = all_responses
+
+            with open(f'output/{languages}/{languages}_split_{split}.json', 'w', encoding="utf-8") as f:
+                json.dump(outputs, f, indent=4, ensure_ascii=False)
